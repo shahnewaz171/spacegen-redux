e530e2e8df1aa07b02dea74fa71e4325423896b2
shahnewaz601 <shahnewaz601@gmail.com>
2021-10-09 22:46:05 +0600

    added conditions for showing upcoming missions

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -4,9 +4,10 @@ import MissionList from '../MissionList/MissionList';
 import { fetchMissions } from './fetchMissionsInfo';
 
 const Missions = () => {
-    const { missions } = useSelector((state) => state.missionsInfo);
+    const { missions, isLoading } = useSelector((state) => state.missionsInfo);
     const [searchValue, setSearchValue] = useState("");
     const [launchStatus, setLaunchStatus] = useState('');
+    const [isUpcoming, setIsUpcoming] = useState('');
 
     const dispatch = useDispatch();
 
@@ -20,19 +21,19 @@ const Missions = () => {
         e.preventDefault();
     }
 
-    const specificMissions = missions?.filter((data, index) => {
+    const specificMissions = missions?.filter((data) => {
         if(searchValue == ""){
             return data;
         }
         else if(data.rocket?.rocket_name.toLowerCase().includes(searchValue.toLowerCase())){
-            if(data.launch_success == true && launchStatus == 'success'){
+            if((data.launch_success == true && launchStatus == 'success') || (data.upcoming == true && isUpcoming == "upcoming")){
                 return data;
             }
             else if(data.launch_success !== null && data.launch_success == false && launchStatus == 'failure'){
                 return data;
             }
-            else if(launchStatus == ''){
-                return data
+            else if(launchStatus == '' && isUpcoming == ""){
+                return data;
             }
         }
     })
@@ -44,9 +45,20 @@ const Missions = () => {
                 <div className="col-6 col-lg-2 pb-2">
                     <div className="d-flex flex-column">
                         <label htmlFor="upcoming">Is upcoming?</label>
-                        <select id="upcoming" className="form-select">
-                            <option defaultValue="">Choose...</option>
-                            <option value="1">Upcoming</option>
+                        <select id="upcoming" onChange={(e) => setIsUpcoming(e.target.value)} className="form-select">
+                            <option value="">Choose...</option>
+                            <option value="upcoming">Upcoming</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="col-6 col-lg-2 pb-2">
+                    <div className="d-flex flex-column">
+                        <label htmlFor="launchStatus">Launch Status</label>
+                        <select id="launchStatus" onChange={(e) => setLaunchStatus(e.target.value)} className="form-select">
+                            <option value="">Choose...</option>
+                            <option value="failure">Failure</option>
+                            <option value="success">Success</option>
                         </select>
                     </div>
                 </div>
@@ -59,17 +71,6 @@ const Missions = () => {
                             <option value="1">Last Week</option>
                             <option value="2">Last Month</option>
                             <option value="3">Last Year</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div className="col-6 col-lg-2 pb-2">
-                    <div className="d-flex flex-column">
-                        <label htmlFor="launchStatus">Launch Status</label>
-                        <select id="launchStatus" onChange={(e) => setLaunchStatus(e.target.value)} className="form-select">
-                            <option value="">Choose...</option>
-                            <option value="failure">Failure</option>
-                            <option value="success">Success</option>
                         </select>
                     </div>
                 </div>
@@ -89,14 +90,15 @@ const Missions = () => {
 
             <div className="mt-4 mx-4">
                 <div className="row row-cols-1 row-cols-md-3 g-4">
-                    {
+                    {/* {
                         specificMissions.length ? specificMissions.map((mission) => {
                             return (
                                 <MissionList mission={mission} key={mission.mission_name} />
                             )
                         })
-                        : <p>No results found for <span className="text-danger">#{searchValue}</span></p>
-                    }
+                        : !isLoading &&
+                        <p>No results found for <span className="text-danger">#{searchValue}</span></p> 
+                    } */}
                 </div>
 
                 <div className="d-flex justify-content-center mt-5">
